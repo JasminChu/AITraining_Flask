5957080d948daba3c13d143e4ced33423e11e0dc
JasminChu <keechu613@gmail.com>
2025-09-03 16:14:59 +0800

    SetUp

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\User\Downloads\Flask\venv
+set VIRTUAL_ENV=C:\Users\User\Documents\GitHub\AITraining_Flask\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
